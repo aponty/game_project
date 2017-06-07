0a1319ae800a51906ff7b4e8d1905f0674f43880
Anson <aponty@gmail.com>
2017-06-07 18:04:11 -0400

    redone in px, added bricks, most of brick collision logic

--- main.js
+++ main.js
@@ -1,30 +1,42 @@
 console.log('linked')
-
-var gameFrame = document.querySelector('.gamewindow'),
-  paddle = document.querySelector('.paddle'),
-  paddleLeft = 0,
-  ball = document.querySelector('.ball'),
-  ballVertPos = 24,
-  ballVertVelocity = 1,
-  ballHorPos = 0,
-  ballHorVelocity = 1;
-
 //TODO: menu. Pause. Final screen. Score keeping (local storage)
-//generate blocks and collision detection (?) or go pong?
-//add spin to ball to erraticise it
-//need to add depending on paddle movement. How to track if it's moving left/rigth/still?
-//if keypress right and ball slope is towards it, multiply <1
-//if keypress right and ball slope is away from it, multiply > 1
-// etc for left
+//generate blocks and collision detection
 //acceleration with time?
-
-
-
 // help with cleaning up oscilation logic from  here http://isaacsukin.com/news/2015/01/detailed-explanation-javascript-game-loops-and-timing
 //and keypress object and controlling everthing in a gameLogic fn set to interval here- https://stackoverflow.com/questions/13538168/is-it-possible-to-make-jquery-keydown-respond-faster
-//make object of keys pressed that only persists while they're pressed. If it's there when setInterval runs, it'll be executed.
-//I really like seting the value = true. clever, nice and clean conditionals.
+
+var gameFrame = document.querySelector('.gamewindow');
+var paddle = document.querySelector('.paddle');
+var paddleLeft = 0;
+var ball = document.querySelector('.ball');
+var ballVertPos = 560;
+var ballVertVelocity = 5;
+var ballHorPos = 0;
+var ballHorVelocity = 5;
+var assets = [{
+    class: 'hash',
+    url: './assets/2hash.png',
+    width: 70
+  },
+  {
+    class: 'amp',
+    url: './assets/3amp.png',
+    width: 140
+  },
+  {
+    class: 'percent',
+    url: './assets/4percent.png',
+    width: 190
+  },
+  {
+    class: 'asterisk',
+    url: './assets/5asterisk.png',
+    width: 300
+  }
+];
 var keys = {}
+
+
 document.addEventListener('keydown', addKey);
 document.addEventListener('keyup', removeKey)
 
@@ -36,75 +48,114 @@ function removeKey(x) {
   delete keys[x.which];
 }
 
-function moveLeft() {
+function movePaddleLeft() {
   if (keys[37] && paddleLeft > 0) {
-    paddleLeft -= 1;
-    paddle.style.left = paddleLeft + '%';
+    paddleLeft -= 5;
+    paddle.style.left = paddleLeft + 'px';
   };
 }
 
-function moveRight() {
-  if (keys[39] && paddleLeft <= 80) {
-    paddleLeft += 1;
-    paddle.style.left = paddleLeft + '%';
+function movePaddleRight() {
+  if (keys[39] && paddleLeft <= 675) {
+    paddleLeft += 5;
+    paddle.style.left = paddleLeft + 'px';
   };
 }
 
 function ballPaddleCollisionCheck() {
-  if (ballVertPos === 100) {
-    if (ballHorPos >= paddleLeft && ballHorPos <= paddleLeft + 40) {
-      console.log('collision! calling spinCheck')
+  if (ballVertPos >= 583) {
+    if (ballHorPos >= paddleLeft && ballHorPos <= paddleLeft + 125) {
       spinCheck();
     }
   }
 }
 
 function spinCheck() {
-  var z = Math.sqrt((Math.pow(ballHorVelocity, 2) + Math.pow(ballVertVelocity, 2) - Math.pow(.75 * ballVertVelocity, 2))/Math.pow(ballHorVelocity, 2))
-  var m = Math.sqrt((Math.pow(ballVertVelocity, 2) + Math.pow(ballHorVelocity, 2) - Math.pow(.75 * ballHorVelocity, 2))/Math.pow(ballVertVelocity, 2))
+  var z = Math.sqrt((Math.pow(ballHorVelocity, 2) + Math.pow(ballVertVelocity, 2) - Math.pow(.75 * ballVertVelocity, 2)) / Math.pow(ballHorVelocity, 2))
+  var m = Math.sqrt((Math.pow(ballVertVelocity, 2) + Math.pow(ballHorVelocity, 2) - Math.pow(.75 * ballHorVelocity, 2)) / Math.pow(ballVertVelocity, 2))
 
   // add an if not on right edge condition or if not on left edge spin
   if (keys[39] && ballHorVelocity > 0) {
     ballVertVelocity *= .75
     ballHorVelocity *= z
-    console.log('spin flat')
+    console.log('spun flat')
   }
   if (keys[39] && ballHorVelocity < 0) {
     ballVertVelocity *= m
     ballHorVelocity *= .75
-    console.log('spin up')
+    console.log('spun up')
   }
   if (keys[37] && ballHorVelocity > 0) {
     ballVertVelocity *= m
     ballHorVelocity *= .75
-    console.log('spin up')
+    console.log('spun up')
   }
   if (keys[37] && ballHorVelocity < 0) {
     ballVertVelocity *= .75
     ballHorVelocity *= z
-    console.log('spin flat')
+    console.log('spun flat')
   }
 }
 
-function oscilateBall() {
-  ball.style.left = ballHorPos + '%';
-  ball.style.top = ballVertPos + '%';
+function moveAndBounceBall() {
+  ball.style.left = ballHorPos + 'px';
+  ball.style.top = ballVertPos + 'px';
   ballHorPos += ballHorVelocity;
   ballVertPos += ballVertVelocity;
-  if (ballHorPos >= 100 || ballHorPos <= 0) {
+  if (ballHorPos <= 0 || ballHorPos >= 794) {  // horizontal
     ballHorVelocity = -ballHorVelocity;
   };
-  if (ballVertPos >= 100 || ballVertPos <= 0) {
+  if (ballVertPos <= 0 || ballVertPos >= 583) { //vertical
     ballVertVelocity = -ballVertVelocity;
+
   };
 }
 
+
+//TODO need to get left + length from array/classname somehow. Then need to add horizontal bounce off the sides. (should be easier)
+function ballBrickCollisionCheck() {
+  document.querySelectorAll('.brick').forEach(x => {
+    var top = parseFloat(x.style.top.split('px')[0])
+    var left = parseFloat(x.style.left.split('px')[0])
+    var right = ''; //something with x.classList[1] and an object
+    if (ballVertPos <= (top + 50) && ballVertPos >= top && ballHorPos >= left && ballHorPos <= left + 200 ) {
+      x.parentNode.removeChild(x);
+      ballVertVelocity = -ballVertVelocity;
+    }
+  })
+}
+
 function gameLoop() {
-  moveLeft();
-  moveRight();
-  oscilateBall();
+  movePaddleLeft();
+  movePaddleRight();
+  moveAndBounceBall();
   ballPaddleCollisionCheck();
+  ballBrickCollisionCheck();
   requestAnimationFrame(gameLoop);
 }
 
 requestAnimationFrame(gameLoop);
+
+
+
+function setBoard() {
+  assets.forEach((x, i) => { //change to some counter that 10x (can increment by level) goes and pulls random index from array
+    var brick = document.createElement('div');
+    brick.setAttribute('class', `brick ${assets[i].class}`);
+    brick.style.backgroundImage = 'url("' + assets[i].url + '")';
+    brick.style.top = Math.random() * 300 + 'px';
+    brick.style.left = Math.random() * (500 - assets[i].width) + 'px';
+    gameFrame.appendChild(brick);
+  })
+}
+setBoard();
+
+
+
+
+
+
+
+
+
+//
