ecfdb74493617221ad522e269295f2fce489b882
Anson <aponty@gmail.com>
2017-06-09 11:02:26 -0400

    start/pause, landing page, media query

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 console.log('linked')
-//TODO: menu. Pause. Final screen. Score keeping (local storage)
-//generate blocks and collision detection
+//TODO: menu. Final screen. Score keeping (local storage)
 //acceleration with time?
 // help with cleaning up oscilation logic from  here http://isaacsukin.com/news/2015/01/detailed-explanation-javascript-game-loops-and-timing
 //and keypress object and controlling everthing in a gameLogic fn set to interval here- https://stackoverflow.com/questions/13538168/is-it-possible-to-make-jquery-keydown-respond-faster
@@ -40,12 +39,13 @@ var widthByClass = {
   percent: 190,
   asterisk: 300
 };
-var keys = {}
+var keys = {};
+var startPause = 'never run';
 
-document.addEventListener('keydown', addKey);
+document.addEventListener('keydown', addKeyAndStartPause);
 document.addEventListener('keyup', removeKey);
+document.querySelector('button').addEventListener('click', clickStart);
 
-//TODO need to add random generation. In batches (levels) or just running.
 function setBoard() {
   var timesToIterateAssets = 10;
   for (var j = 0; j < timesToIterateAssets; j++) {
@@ -53,75 +53,91 @@ function setBoard() {
       var brick = document.createElement('div');
       brick.setAttribute('class', `brick ${assets[i].class}`);
       brick.style.backgroundImage = 'url("' + assets[i].url + '")';
-      brick.style.top = Math.random() * 300 + 'px';
+      brick.style.top = Math.random() * 275 + 'px';
       brick.style.left = Math.random() * (800 - assets[i].width) + 'px';
       gameFrame.appendChild(brick);
     }
   }
 };
 
-setBoard();
+function addKeyAndStartPause(x) {
+  keys[x.which] = true; //adds keys to keys object (keylogger)
 
-function addKey(x) {
-  keys[x.which] = true;
-}
+  if (x.which === 32) { //start/pause w/ spacebar. If this listener callback grows I'll separate things out
+    if (startPause === 'never run') {
+      startGame();
+      startPause = 'run';
+    } else if (startPause === 'run') {
+      startPause = 'stop';
+    } else if (startPause === 'stop') {
+      startPause = 'run';
+    }
+  }
+};
+
+function clickStart() {
+  if (startPause === 'never run') {
+    startGame();
+    startPause = 'run';
+  }
+};
 
 function removeKey(x) {
   delete keys[x.which];
-}
+};
 
 function movePaddleLeft() {
   if (keys[37] && paddleLeft > 0) {
-    paddleLeft -= 5;
+    paddleLeft -= 7;
     paddle.style.left = paddleLeft + 'px';
   };
-}
+};
 
 function movePaddleRight() {
   if (keys[39] && paddleLeft <= 675) {
-    paddleLeft += 5;
+    paddleLeft += 7;
     paddle.style.left = paddleLeft + 'px';
   };
-}
+};
 
 function ballPaddleCollisionCheck() {
   if (ballVertPos === 585) {
     if (ballHorPos >= paddleLeft && ballHorPos <= paddleLeft + 125) {
-      console.log('call spincheck')
+      console.log('collision! call spincheck');
       spinCheck();
     }
   }
-}
+};
 
 function spinCheck() {
-  var z = Math.sqrt((Math.pow(ballHorVelocity, 2) + Math.pow(ballVertVelocity, 2) - Math.pow(.75 * ballVertVelocity, 2)) / Math.pow(ballHorVelocity, 2))
-  var m = Math.sqrt((Math.pow(ballVertVelocity, 2) + Math.pow(ballHorVelocity, 2) - Math.pow(.75 * ballHorVelocity, 2)) / Math.pow(ballVertVelocity, 2))
+  var z = Math.sqrt((Math.pow(ballHorVelocity, 2) + Math.pow(ballVertVelocity, 2) - Math.pow(.75 * ballVertVelocity, 2)) / Math.pow(ballHorVelocity, 2));
+  var m = Math.sqrt((Math.pow(ballVertVelocity, 2) + Math.pow(ballHorVelocity, 2) - Math.pow(.75 * ballHorVelocity, 2)) / Math.pow(ballVertVelocity, 2));
 
   //if paddle right and ball right
   if (keys[39] && paddleLeft < 675 && ballHorVelocity > 0) {
-    ballVertVelocity *= .75
-    ballHorVelocity *= z
-    console.log('spun flat')
+    ballVertVelocity *= .75;
+    ballHorVelocity *= z;
+    console.log('spun flat');
   }
   //if paddle right and ball left
   if (keys[39] && paddleLeft < 675 && ballHorVelocity < 0) {
-    ballVertVelocity *= m
-    ballHorVelocity *= .75
-    console.log('spun up')
+    ballVertVelocity *= m;
+    ballHorVelocity *= .75;
+    console.log('spun up');
   }
   //if paddle left and ball right
   if (keys[37] && paddleLeft >= 1 && ballHorVelocity > 0) {
-    ballVertVelocity *= m
-    ballHorVelocity *= .75
-    console.log('spun up')
+    ballVertVelocity *= m;
+    ballHorVelocity *= .75;
+    console.log('spun up');
   }
   //if paddle left and ball left
   if (keys[37] && paddleLeft >= 1 && ballHorVelocity < 0) {
-    ballVertVelocity *= .75
-    ballHorVelocity *= z
-    console.log('spun flat')
+    ballVertVelocity *= .75;
+    ballHorVelocity *= z;
+    console.log('spun flat');
   }
-}
+};
 
 function moveAndBounceBall() {
   ball.style.left = ballHorPos + 'px';
@@ -133,20 +149,19 @@ function moveAndBounceBall() {
   };
   if (ballVertPos <= 0 || ballVertPos >= 585) { //vertical
     ballVertVelocity = -ballVertVelocity;
-
   };
-}
+};
 
 function ballBrickCollisionCheck() {
   document.querySelectorAll('.brick').forEach(x => {
-    var top = parseFloat(x.style.top.split('px')[0])
-    var left = parseFloat(x.style.left.split('px')[0])
-    var right = widthByClass[x.classList[1]]
+    var top = parseFloat(x.style.top.split('px')[0]);
+    var left = parseFloat(x.style.left.split('px')[0]);
+    var right = widthByClass[x.classList[1]]; //data attributes would be much cleaner. But this works, and priorities say new features are more important
     //left edge. Sequence of elses is arbitrary; leaving them as naked ifs led to multiple delete attempts on some corner strikes
     if (ballHorPos >= left && ballHorPos <= (left + 10) && ballVertPos >= top && ballVertPos <= (top + 50)) {
       x.parentNode.removeChild(x);
       ballHorVelocity = -ballHorVelocity;
-    } else //bottom edge. Need to pick edges specifically to bounce horizontal/vert. === to bottom edge gets jumped over. Added 10px fuzz
+    } else //bottom edge. Need to pick edges specifically to bounce horizontal vs vert. === to bottom edge gets jumped over. Added 10px fuzz
       if (ballVertPos <= (top + 50) && ballVertPos >= (top + 40) && ballHorPos >= left && ballHorPos <= (left + right)) {
         x.parentNode.removeChild(x);
         ballVertVelocity = -ballVertVelocity;
@@ -160,26 +175,25 @@ function ballBrickCollisionCheck() {
             ballVertVelocity = -ballVertVelocity;
           }
   })
-}
+};
 
 function gameLoop() {
-  movePaddleLeft();
-  movePaddleRight();
-  moveAndBounceBall();
-  ballPaddleCollisionCheck();
-  ballBrickCollisionCheck();
+  if (startPause === 'run') {
+    movePaddleLeft();
+    movePaddleRight();
+    moveAndBounceBall();
+    ballPaddleCollisionCheck();
+    ballBrickCollisionCheck();
+  }
   requestAnimationFrame(gameLoop);
-}
+};
 
-// requestAnimationFrame(gameLoop);
-// var gameStart = setInterval(gameLoop, 17)
-
-
-
-
-
-
-
-
-
-//
+function startGame() {
+  document.querySelector('.landing').style.display = 'none'
+  paddle.style.display = 'block';
+  ball.style.display = 'block';
+  setBoard();
+  requestAnimationFrame(gameLoop);
+  // var gameStart = setInterval(gameLoop, 17)  alt start for leveling. Can easily change speed w/out affecting math, but laggy
+};
+// startGame(); second part of comment above
